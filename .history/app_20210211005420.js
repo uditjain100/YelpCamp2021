4bc5c184ad3eefcc17bba9e6365e4afc7dec9050
Udit Jain <jain30udit@gmail.com>
2021-02-11 01:04:13 +0530

    routers

---
+++ .history/app_20210211005420.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const methodoverride = require("method-override");
+const ejsMate = require("ejs-mate");
+
+const ExpressError = require("./ulits/ExpressError");
+
+const campgrounds = require("./routers/campgrounds");
+const reviews = require("./routers/reviews");
+
+mongoose
+  .connect("mongodb://localhost:27017/yelp-camp", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database got Connected (:)");
+  })
+  .catch((error) => {
+    console.log("Oh No ERROR ::(");
+    console.log(error);
+  });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodoverride("_method"));
+
+app.use("/campgrounds", campgrounds);
+app.use("/campgrounds/:id/reviews", reviews);
+
+app.engine("ejs", ejsMate);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found :("));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("./campground/error.ejs", { err });
+});
+
+app.listen(3000, () => {
+  console.log("Server got started !!");
+});
---
+++ .history/routers/campgrounds_20210210233125.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const router = express.Router();
+const Campground = require("../models/campground");
+
+const { campgroundSchema } = require("../ulits/validateSchemas");
+const catchAsyncError = require("../ulits/CatchAsyncError");
+const ExpressError = require("../ulits/ExpressError");
+
+const validateCampgroundSchema = (req, res, next) => {
+  var validationResult = campgroundSchema.validate(req.body);
+  if (validationResult.error) {
+    const errorMessage = validationResult.error.details
+      .map((ele) => ele.message)
+      .join(",");
+    throw new ExpressError(504, errorMessage);
+  } else {
+    next();
+  }
+};
+
+router.get("/home", (req, res) => {
+  res.render("./campground/home.ejs");
+});
+
+router.get(
+  "/",
+  catchAsyncError(async (req, res) => {
+    console.log("Heyyyyyyy Home page reached... !!");
+    var campgrounds = await Campground.find({});
+    res.render("./campground/campgrounds.ejs", { campgrounds });
+  })
+);
+
+router.get(
+  "/farms",
+  catchAsyncError(async (req, res) => {
+    res.render("./campground/newCamp.ejs");
+  })
+);
+
+router.get(
+  "/:id",
+  catchAsyncError(async (req, res) => {
+    var { id } = req.params;
+    var camp = await Campground.findById(id).populate("reviews");
+    res.render("./campground/details.ejs", { camp });
+  })
+);
+
+router.post(
+  "/",
+  validateCampgroundSchema,
+  catchAsyncError(async (req, res, next) => {
+    var c = new Campground(req.body.campground);
+    await c.save();
+    res.redirect("/campgrounds");
+  })
+);
+
+router.get(
+  "/:id/update",
+  catchAsyncError(async (req, res) => {
+    var { id } = req.params;
+    const camp = await Campground.findById(id);
+    res.render("./campground/update.ejs", { camp });
+  })
+);
+
+router.patch(
+  "/:id",
+  validateCampgroundSchema,
+  catchAsyncError(async (req, res) => {
+    var { id } = req.params;
+    var { title, price, description, latitude, longitude } = req.body;
+    await Campground.findByIdAndUpdate(id, {
+      title: title,
+      price: price,
+      description: description,
+      location: "" + latitude + " , " + longitude,
+    });
+    const camp = await Campground.findById(id);
+    res.render("./campground/details.ejs", { camp });
+  })
+);
+
+router.delete(
+  "/:id",
+  catchAsyncError(async (req, res) => {
+    var { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    var campgrounds = await Campground.find({});
+    res.render("./campground/campgrounds.ejs", { campgrounds });
+  })
+);
+
+module.exports = router;
