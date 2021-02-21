7355a50349d7738805c2c6d550d3918096f1fc51
Udit Jain <jain30udit@gmail.com>
2021-02-21 17:07:23 +0530

    dark-mode

--- .history/public/javascripts/mapCluster_20210221170415.js
+++ .history/public/javascripts/mapCluster_20210221170415.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 var map = new mapboxgl.Map({
   container: "map",
-  style: "mapbox://styles/mapbox/dark-v10",
+  style: "mapbox://styles/mapbox/light-v10",
   center: [-103.59179687498357, 40.66995747013945],
   zoom: 3,
 });
